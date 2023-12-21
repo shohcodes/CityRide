428d15d786c08c578511b5ae368c64a09b06f296
shohcodes <zokirislom01@gmail.com>
2023-12-21 16:31:35 +0500

    base urls

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-# from rest_framework_simplejwt.views import TokenRefreshView
 
 from apps.users.views import get_me
 
 urlpatterns = [
-    # path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/get_me/', get_me, name='get_me')
 ]
